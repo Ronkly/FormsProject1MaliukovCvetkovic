64f9cb5f2096156e13270930e53091b726c9f9ae
Ronkly <111786896+Ronkly@users.noreply.github.com>
2025-12-10 13:56:50 +0100

    Update TasksForm.cs

--- FormsProject1Maliukov/Views/TasksForm.cs
+++ FormsProject1Maliukov/Views/TasksForm.cs
@@ -13,27 +13,16 @@ namespace FormsProject1MaliukovCvetkovic.Views
 
         private void LoadData()
         {
-            // I'll set the welcome label text here
             labelWelcome.Text = $"Hello, {Classes.DataBase.CurrentUser.Name}";
 
             RefreshTaskLists();
-
-            // I'll populate the ComboBox with authors for filtering here
         }
 
-        // I'll call this after adding/deleting a task or on the Refresh button click
         private void RefreshTaskLists()
         {
-            // 1. Personal tasks (owner_ID == CurrentUser.Id)
             List<Classes.Task> myTasks = Classes.DataBase.tasks.Where(t => t.owner_ID == Classes.DataBase.CurrentUser.Id).ToList();
-            // I'll bind the data to dataGridViewMyTasks here
-
-            // 2. All company tasks (filtered/sorted as needed)
             List<Classes.Task> allTasks = Classes.DataBase.tasks.ToList();
-            // I'll bind the data to dataGridViewAllTasks here
         }
-
-        // I'll call this when the "Create Task" button is confirmed
         private void AddNewTask(string title, string desc, DateTime date)
         {
             if (Classes.DataBase.CurrentUser == null)
@@ -47,8 +36,6 @@ namespace FormsProject1MaliukovCvetkovic.Views
 
             RefreshTaskLists();
         }
-
-        // I'll call this from the DataGridView CellContentClick event when the Delete button is pressed
         private void DeleteSelectedTask(Classes.Task taskToDelete)
         {
             bool isAdmin = Classes.DataBase.CurrentUser is Classes.AdminUser;
@@ -66,17 +53,12 @@ namespace FormsProject1MaliukovCvetkovic.Views
                 _ = MessageBox.Show("You cannot delete someone else's task.");
             }
         }
-
-        // I'll call this from the Log Out button
-        private void PerformLogout()
         {
             Classes.DataBase.CurrentUser = null;
             LoginForm login = new();
             login.Show();
             Close();
         }
-
-        // I'll call this from the "View All Workers" button
         private void OpenUserManagement()
         {
             if (Classes.DataBase.CurrentUser is Classes.AdminUser)
@@ -90,43 +72,35 @@ namespace FormsProject1MaliukovCvetkovic.Views
                 _ = MessageBox.Show("This feature is only available to the Administrator.");
             }
         }
-
-        // Search box for tasks
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        // Grid for all tasks
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
-        // Grid for my tasks
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
-        // Here I need to add the choice of the sorting (alphabetical, date)
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
-        // Button for opening the list of all users (everyone can open it, just admins can manage it)
         private void buttonUserList_Click(object sender, EventArgs e)
         {
             OpenUserManagement();
         }
 
-        // Log out button
         private void buttonLogOut_Click(object sender, EventArgs e)
         {
             PerformLogout();
         }
 
-        // Profile button
         private void buttonProfile_Click(object sender, EventArgs e)
         {
 
